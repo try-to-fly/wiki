990243d90ceef3e59381c715781d3c683bcdf58a
微笑 <wang1234561211@outlook.com>
2023-04-28 17:15:03 +0800

    feat: 优化工具页布局

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -11,8 +11,9 @@ import {
   BulbOutlined,
   CoffeeOutlined,
   BookOutlined,
-  AppstoreOutlined,
+  QrcodeOutlined,
   HighlightOutlined,
+  RestOutlined,
 } from "@ant-design/icons";
 import { Wrap } from "@site/src/component/Wrap";
 
@@ -21,116 +22,150 @@ interface NavItem {
   url: string;
   gradient: string;
   icon: React.ReactNode;
+  type: NavigationType;
 }
 
-/**
-linear-gradient(135deg, #F2BAC9, #F5D5D7)
-linear-gradient(135deg, #F3D3BD, #F5E2D5)
-linear-gradient(135deg, #C2D4E8, #E3EFF9)
-linear-gradient(135deg, #E4D4C2, #F9ECE1)
-linear-gradient(135deg, #FBE7CB, #F9F9F9)
-linear-gradient(135deg, #FCD2C2, #FFE7E7)
-linear-gradient(135deg, #B5EAD7, #E1F8F7)
-linear-gradient(135deg, #D9B9E8, #F3E4F9)
-linear-gradient(135deg, #FBE5C8, #F9F9F9)
-linear-gradient(135deg, #B5E5AB, #E1F9E2)
-linear-gradient(135deg, #E8D2B2, #F9EFE5)
-linear-gradient(135deg, #D6C7F9, #F0E7FF)
-linear-gradient(135deg, #FAC8B8, #FEE3D3)
-linear-gradient(135deg, #FBECC8, #F9F9F9)
-linear-gradient(135deg, #B5D5E5, #E1F1F9)
-*/
-const navigationData = [
+enum NavigationType {
+  Time = "时间相关",
+  Image = "图片相关",
+  Tool = "工具相关",
+}
+type Category = {
+  title: string;
+  items: NavItem[];
+};
+
+const navigationData: NavItem[] = [
   {
     name: "时间进度",
     url: "/today",
     gradient: "linear-gradient(135deg, #6EE7B7 0%, #3FA7D6 100%)",
     icon: <SmileOutlined />,
+    type: NavigationType.Time,
   },
   {
     name: "图片水印",
     url: "/watermark",
     gradient: "linear-gradient(135deg, #FDD819 0%, #FF8F00 100%)",
     icon: <FileImageOutlined />,
+    type: NavigationType.Image,
   },
   {
     name: "全景图",
     url: "/PanoramaViewer",
     gradient: "linear-gradient(135deg, #F86D77 0%, #F88D50 50%, #FFBF00 100%)",
     icon: <EyeOutlined />,
+    type: NavigationType.Image,
   },
   {
     name: "图片去背景",
     url: "/pureImage",
     gradient: "linear-gradient(135deg, #F97794 0%, #623AA2 100%)",
     icon: <ThunderboltOutlined />,
+    type: NavigationType.Image,
   },
   {
     name: "URL 解析",
     url: "/url-parse",
     gradient: "linear-gradient(135deg, #36D1DC 0%, #5B86E5 100%)",
     icon: <IeOutlined />,
+    type: NavigationType.Tool,
   },
   {
     name: "json工具",
     url: "/json",
     gradient: "linear-gradient(135deg, #FFC107 0%, #FF9800 50%, #FF5722 100%)",
     icon: <BulbOutlined />,
+    type: NavigationType.Tool,
   },
   {
     name: "时间戳",
     url: "/timestamp",
     gradient: "linear-gradient(135deg, #9C27B0 0%, #E91E63 50%, #F44336 100%)",
     icon: <CoffeeOutlined />,
+    type: NavigationType.Time,
   },
   {
     name: "剪切板",
     url: "/clipboard",
     gradient: "linear-gradient(135deg, #F0E0A4, #F9F5E3)",
     icon: <BookOutlined />,
+    type: NavigationType.Tool,
   },
   {
     name: "二维码",
     url: "/qrcode",
     gradient: "linear-gradient(135deg, #D8B1E8, #F3DCEC)",
-    icon: <AppstoreOutlined />,
+    icon: <QrcodeOutlined />,
+    type: NavigationType.Image,
   },
   {
     name: "文本提取",
     url: "/textmatch",
     gradient: "linear-gradient(135deg, #F9E9C8, #FFF9F9)",
     icon: <HighlightOutlined />,
+    type: NavigationType.Tool,
+  },
+  {
+    name: "Base64编解码",
+    url: "/base64",
+    gradient: "linear-gradient(135deg, #D6C7F9, #F0E7FF)",
+    icon: <RestOutlined />,
+    type: NavigationType.Tool,
   },
 ];
+
+function groupItemsByType(items: NavItem[]): Category[] {
+  const groupedItems: { [key in NavigationType]?: NavItem[] } = {};
+
+  items.forEach((item) => {
+    if (!groupedItems[item.type]) {
+      groupedItems[item.type] = [];
+    }
+    groupedItems[item.type].push(item);
+  });
+
+  return Object.entries(groupedItems).map(([key, items]) => ({
+    title: key,
+    items,
+  }));
+}
+
+const groupedNavigationData: Category[] = groupItemsByType(navigationData);
 
 const Navigation: React.FC = () => {
   return (
     <Wrap gptUrl="https://sharegpt.com/c/rkKOVvF">
       <div className={styles.navigationWrapper}>
-        <Row gutter={[24, 24]} justify="center">
-          {navigationData.map((item: NavItem) => (
-            <Col
-              key={item.name}
-              xs={24}
-              sm={12}
-              md={8}
-              lg={6}
-              xl={4}
-              style={{ display: "flex", justifyContent: "center" }}
-            >
-              <Link
-                to={item.url}
-                className={styles.navigationItem}
-                style={{ backgroundImage: item.gradient }}
-              >
-                <div className={styles.cardIcon}>{item.icon}</div>
-                <div className={styles.cardContent}>
-                  <div className={styles.cardTitle}>{item.name}</div>
-                </div>
-              </Link>
-            </Col>
-          ))}
-        </Row>
+        {groupedNavigationData.map((category: Category) => (
+          <div className={styles.category} key={category.title}>
+            <h2>{category.title}</h2>
+            <Row gutter={[24, 24]}>
+              {category.items.map((item: NavItem) => (
+                <Col
+                  key={item.name}
+                  xs={24}
+                  sm={12}
+                  md={8}
+                  lg={6}
+                  xl={4}
+                  style={{ display: "flex", justifyContent: "left" }}
+                >
+                  <Link
+                    to={item.url}
+                    className={styles.navigationItem}
+                    style={{ backgroundImage: item.gradient }}
+                  >
+                    <div className={styles.cardIcon}>{item.icon}</div>
+                    <div className={styles.cardContent}>
+                      <div className={styles.cardTitle}>{item.name}</div>
+                    </div>
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        ))}
       </div>
     </Wrap>
   );
