84217261bbfce1e406b9ee562b88000739c905b1
微笑 <wang1234561211@outlook.com>
2023-05-17 11:28:26 +0800

    fix: 避免重复轮训sse接口

--- pages/demo/sse.tsx
+++ pages/demo/sse.tsx
@@ -5,9 +5,11 @@ export default function Home() {
 
   useEffect(() => {
     const source = new EventSource("/api/demo/sse");
-
     source.onmessage = function (event) {
       setEvents((oldEvents) => [...oldEvents, event.data]);
+      if (event.data.startsWith("5")) {
+        source.close();
+      }
     };
 
     return () => {
@@ -18,6 +20,10 @@ export default function Home() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Server-Sent Events Demo</h1>
+      <h1>
+        注意：由于serveless
+        function的限制，sse发送的消息只能在end事件后，合并获取到所有内容。
+      </h1>
       {events.map((event, index) => (
         <p key={index}>{event}</p>
       ))}
