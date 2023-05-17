9a03fe9d8240b66c9ac50b8281cbbd738ff1dff0
微笑 <wang1234561211@outlook.com>
2023-05-17 10:45:16 +0800

    fix: 避免sse一次性发送全部消息

--- pages/api/demo/sse.ts
+++ pages/api/demo/sse.ts
@@ -4,8 +4,11 @@ const maxEvent = 5;
 // pages/api/events.js
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // 设置HTTP头以启用SSE
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "text/event-stream;charset=utf-8");
+  res.setHeader("Cache-Control", "no-cache, no-transform");
+  res.setHeader("X-Accel-Buffering", "no");
+  res.setHeader("Content-Encoding", "none");
   res.setHeader("Connection", "keep-alive");
   let i = 0;
   // 每秒发送一个新的事件
