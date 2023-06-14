36beeeb5ab960843e064e8d69279be4c1b91d122
微笑 <wang1234561211@outlook.com>
2023-06-14 11:13:08 +0800

    --wip-- [skip ci]

--- components/ScenicSpot/index.tsx
+++ components/ScenicSpot/index.tsx
@@ -1,6 +1,15 @@
-// ScenicSpotDisplay.tsx
 import React from "react";
-import { Card, Row, Col, Rate, Tag, Typography, Carousel, Image } from "antd";
+import {
+  Card,
+  Row,
+  Col,
+  Rate,
+  Tag,
+  Typography,
+  Carousel,
+  Image,
+  Space,
+} from "antd";
 import { PanoramaCanvas } from "../Panorama";
 import styles from "./ScenicSpotDisplay.module.scss";
 import { ScenicSpot } from "./type";
@@ -14,9 +23,11 @@ export const ScenicSpotDisplay = ({
   scenicSpot: ScenicSpot;
 }) => (
   <div className={styles.scenicSpotDisplay}>
-    <Row gutter={[16, 16]}>
-      <Col xs={24} lg={12}>
+    <Row gutter={[16, 16]} justify="start">
+      <Col xs={24} sm={12} md={8} lg={12}>
         <Card
+          size="small"
+          title="图片"
           cover={
             <Carousel autoplay>
               {scenicSpot.imageTextList.map((item, index) => (
@@ -34,37 +45,22 @@ export const ScenicSpotDisplay = ({
           <Tag>{scenicSpot.bestSeason}</Tag>
         </Card>
       </Col>
-      <Col xs={24} lg={12}>
-        {scenicSpot.panoramaList.map((item) => (
-          <PanoramaCanvas key={item.imageUrl} url={item.imageUrl} />
-        ))}
+      <Col xs={24} sm={12} md={8} lg={12}>
+        <Card title="全景图" size="small">
+          <Space direction="vertical" style={{ width: "100%" }}>
+            {scenicSpot.panoramaList.map((item) => (
+              <PanoramaCanvas key={item.imageUrl} url={item.imageUrl} />
+            ))}
+          </Space>
+        </Card>
       </Col>
-    </Row>
-    <Row gutter={[16, 16]}>
-      <Col xs={24}>
-        <Card title="Videos">
+      <Col xs={24} sm={12} md={8} lg={6}>
+        <Card title="Videos" size="small">
           {scenicSpot.videoList.map((video, index) => (
             <video controls key={index} src={video.videoUrl} />
           ))}
         </Card>
       </Col>
     </Row>
-    {/* <Row gutter={[16, 16]}> */}
-    {/*   <Col xs={24}> */}
-    {/*     <Card title="Map"> */}
-    {/*       <MapMarker coordinates={scenicSpot.coordinates} /> */}
-    {/*       {scenicSpot.routeMaps.map((route, index) => ( */}
-    {/*         <MapMarker key={index} coordinatesList={route.coordinatesList} /> */}
-    {/*       ))} */}
-    {/*       {scenicSpot.pointsOfInterest.map((point, index) => ( */}
-    {/*         <MapMarker */}
-    {/*           key={index} */}
-    {/*           coordinates={point.coordinates} */}
-    {/*           type={point.type} */}
-    {/*         /> */}
-    {/*       ))} */}
-    {/*     </Card> */}
-    {/*   </Col> */}
-    {/* </Row> */}
   </div>
 );
