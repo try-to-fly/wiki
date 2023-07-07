7a016d178c33575df29a4cf1a7f96197fa793f23
微笑 <wang1234561211@outlook.com>
2023-07-07 13:53:22 +0800

    fix: 修复icp报错

--- pages/tools/icp/index.tsx
+++ pages/tools/icp/index.tsx
@@ -1,45 +1,41 @@
 import React, { useState } from "react";
-import {
-  Layout,
-  Input,
-  Button,
-  Row,
-  Col,
-  Card,
-  Descriptions,
-  List,
-} from "antd";
+import { Layout, Input, Button, Card, Descriptions, List } from "antd";
 import ky from "ky";
 import { Wrap } from "@components/Wrap";
 import styles from "./styles.module.scss";
+import { AudioOutlined } from "@ant-design/icons";
 
 const { Content } = Layout;
 
 const DomainInfo = ({ data }) => {
-  const { subject, website, whois, dns } = data[0];
+  const { subject, website, whois, dns } = data[0] || data;
 
   return (
     <div className={styles.container}>
-      <Card title="主题">
-        <Descriptions>
-          <Descriptions.Item label="名称">{subject.name}</Descriptions.Item>
-          <Descriptions.Item label="性质">{subject.nature}</Descriptions.Item>
-          <Descriptions.Item label="许可证">
-            {subject.license}
-          </Descriptions.Item>
-          <Descriptions.Item label="更新时间">
-            {subject.updateTime}
-          </Descriptions.Item>
-        </Descriptions>
-      </Card>
-      <Card title="网站">
-        <Descriptions>
-          <Descriptions.Item label="域名">{website.domain}</Descriptions.Item>
-          <Descriptions.Item label="许可证">
-            {website.license}
-          </Descriptions.Item>
-        </Descriptions>
-      </Card>
+      {subject && (
+        <Card title="主题">
+          <Descriptions>
+            <Descriptions.Item label="名称">{subject.name}</Descriptions.Item>
+            <Descriptions.Item label="性质">{subject.nature}</Descriptions.Item>
+            <Descriptions.Item label="许可证">
+              {subject.license}
+            </Descriptions.Item>
+            <Descriptions.Item label="更新时间">
+              {subject.updateTime}
+            </Descriptions.Item>
+          </Descriptions>
+        </Card>
+      )}
+      {website && (
+        <Card title="网站">
+          <Descriptions>
+            <Descriptions.Item label="域名">{website.domain}</Descriptions.Item>
+            <Descriptions.Item label="许可证">
+              {website.license}
+            </Descriptions.Item>
+          </Descriptions>
+        </Card>
+      )}
       <Card title="Whois">
         <List
           dataSource={Object.entries(whois)}
@@ -77,27 +73,30 @@ const DomainInfo = ({ data }) => {
 };
 
 const Icp: React.FC = () => {
-  const [domain, setDomain] = useState<string>("");
   const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const url = new URL(e.target.value);
-    setDomain(url.hostname);
-  };
-
-  const handleSearch = async () => {
-    const result = await ky.get(`/api/proxy/icp?domain=${domain}`).json();
-    setData(result);
+  const handleSearch = async (value: string) => {
+    const url = new URL(value);
+    setLoading(true);
+    try {
+      const result = await ky
+        .get(`/api/proxy/icp?domain=${url.hostname}`)
+        .json();
+      setData(result);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Wrap>
       <div className={styles["search-input"]}>
-        <Input
+        <Input.Search
+          loading={loading}
           placeholder="输入网址"
-          onChange={handleInputChange}
-          suffix={<Button onClick={handleSearch}>查询</Button>}
-        />{" "}
+          onSearch={handleSearch}
+        />
       </div>
       <Content>{data && <DomainInfo data={data} />}</Content>
     </Wrap>
